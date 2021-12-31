8955ae77607771eeb9c0558e9935ec5b6fe5da96
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-12-31 05:24:59 +0300

    update install.js
    Automatically committed by GitBurner

---
+++ install.js
@@ -0,0 +1,15 @@
+let basePath = "https://raw.githubusercontent.com/kotakotik22/GitBurner/main/"
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	let files = JSON.parse(await (await fetch(basePath + "files.txt")).text())
+
+	for (let file of files) {
+		if (!
+			await ns.wget(basePath + file, "gitburner/" + file, "home")
+		) {
+			ns.tprint("Could not download file " + file)
+			return
+		}
+	}
+}
